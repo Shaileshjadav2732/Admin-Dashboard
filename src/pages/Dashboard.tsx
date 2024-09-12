269e615a978efac9fedd9f15138a1f4602a2af28
Shailesh Jadav <jshailesh798@gmail.com>
2024-09-12 13:25:36 +0530

    add widgetItems in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,57 @@
-import { lazy } from 'react'
+import { lazy } from "react";
+import { BsSearch } from "react-icons/bs";
+import { FaRegBell } from "react-icons/fa";
+import userImage from "../assets/userpic.png";
+import { HiTrendingUp,HiTrendingDown } from "react-icons/hi";
+const AdminSidebar = lazy(() => import("../components/AdminSidebar"));
 
-const AdminSidebar =lazy(()=>import('../components/AdminSidebar')) 
 const Dashboard = () => {
-
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main>njnk</main>
+      <main className="dashboard">
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data,users,docs" />
+          <FaRegBell />
+          <img src={userImage} />
+        </div>
+      </main>
+      <section className="widgetContainer">
+        <WidgetItem percent={40} amount={true} value={2732} heading="Revenue" color="rgb(0,155,255)"/>
+      </section>
     </div>
   );
-}
+};
 
-export default Dashboard
+interface widgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
+}
+const WidgetItem = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: widgetItemProps) => {
+ return (<article className="widget">
+    <div className="widgetInfo">
+      <p>{heading}</p>
+      <h4>{amount ? `$${value}` : value}</h4>
+      {percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp /> + {percent}%{" "}
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown /> {percent}%{" "}
+        </span>
+      )}
+    </div>
+  </article>)
+};
+export default Dashboard;
