e40847d5db09429152df72243860a9fcafeeb5ae
Shailesh Jadav <jshailesh798@gmail.com>
2024-09-29 02:02:09 +0530

    add Transaction Management

--- src/pages/management/TransactionManagement.tsx
+++ src/pages/management/TransactionManagement.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import AdminSidebar from "../../components/AdminSidebar";
+import { OrderItemIype, OrderType } from "../../styles/types";
+import { Link } from "react-router-dom";
+
+// Sample order items
+const orderItems: OrderItemIype[] = [
+  {
+    name: "puma shoes",
+    quantity: 2,
+    price: 1000,
+    photo:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTAp7qFXOdCuXbeEaI6mgU0zKKAiUi6n-0p6UrZZ4hvOSuvRNySRsHp03nCxVzUzMEeZOONCCQBJeP6MszHtAw1WXCrKb7fLzopUbbP-Dc9kINy1IUsXa74y2TB&usqp=CAU",
+  },
+];
 
 const TransactionManagement = () => {
-  return (
-    <div>TransactionManagement</div>
-  )
-}
+  const [order, setOrder] = useState<OrderType>({
+    name: "shailesh Jadav",
+    email: "kWQp3@example.com",
+    address: {
+      street: "Halvad",
+      city: "Morbi",
+      state: "Gujarat",
+      country: "India",
+      pincode: 363330,
+      phone: "9876543210",
+    },
+    status: "processing",
+    subtotal: 100,
+    discount: 10,
+    tax: 10,
+    total: 100,
+    orderItems: orderItems,
+    _id: "bivhufujvu",
+  });
 
-export default TransactionManagement
+  const { name, email, address, status, subtotal, discount, tax, total, _id } =
+    order;
+
+  return (
+    <div className="admin-container">
+      <AdminSidebar />
+      <main className="product-management">
+        <section>
+          <h2>Order Item</h2>
+          {orderItems.map((item, index) => (
+            <OrderItemCard key={index} {...item} _id={_id} />
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+// Define OrderItemCard component and pass _id as prop
+const OrderItemCard = ({
+  name,
+  photo,
+  quantity,
+  price,
+  _id,
+}: OrderItemIype & { _id: string }) => {
+  return (
+    <div className="transaction-product-card">
+      <img src={photo} alt={name} />
+      <Link to={`/product/${_id}`}>{name}</Link>
+      <p>Quantity: {quantity}</p>
+      <p>Price: {price}</p>
+    </div>
+  );
+};
+
+export default TransactionManagement;
---
+++ src/styles/types.ts
@@ -0,0 +1,26 @@
+export type OrderItemIype = {
+  name: string;
+  quantity: number;
+  price: number;
+  photo: string;
+};
+
+export type OrderType = {
+  name: string;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    phone: string;
+    pincode: number;
+  };
+  status: "processing" | "shipped" | "delivered";
+  subtotal: number;
+  discount: number;
+  tax: number;
+  total: number;
+  orderItems: OrderItemIype[];
+  _id: string;
+};
