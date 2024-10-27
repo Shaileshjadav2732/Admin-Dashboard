2b9e654ee3d9b4e17ac895f6085e0edb4030ce49
Shailesh Jadav <jshailesh798@gmail.com>
2024-10-27 21:58:46 +0530

    add Toss

--- src/pages/apps/Toss.tsx
+++ src/pages/apps/Toss.tsx
@@ -1,11 +1,29 @@
-
 import AdminSidebar from "../../components/AdminSidebar";
+import { useState } from "react";
 const Toss = () => {
+
+  const [angle,setAngle]=useState(0);
+  const flipCoin = () => {
+    const toss = Math.floor(Math.random() * 2);
+  if(toss===0){
+    setAngle((prev)=>prev+180);
+  }else{
+    setAngle((prev)=>prev+360);
+  }
+  };
   return (
     <div className="admin-container">
       <AdminSidebar />
-      <main>
-         ijrijhr
+      <main className="dashboard-app-container">
+        <h1>Toss</h1>
+        <section>
+          <article className="tosscoin" onClick={flipCoin}
+         
+            style={{ transform: `rotateY(${angle}deg)` }}>
+            <div></div>
+            <div></div>
+          </article>
+        </section>
       </main>
     </div>
   );
